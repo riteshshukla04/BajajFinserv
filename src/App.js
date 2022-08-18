0d09e705dae3da3b7097d17c2c17b5ea30d5f446
Ritesh <riteshshukla2381@gmail.com>
2022-08-18 13:13:54 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo,setTodo]=useState([{
+    "Title":"Coding ",
+    "Content":"Code a React App"
+  }]);
+
+
+  const [text,setText]=useState("")
+  const [content,setContent]=useState("")
+  const[editable,setEditable]=useState(false)
+  const [edit,setEdit]=useState(false)
+  const AddText=()=>{
+    if(text && content){
+      const obj={
+        "Title":text,
+        "Content":content
+      }
+      setTodo([...todo,obj]);
+    }
+  }
+  const DeleteTodo=(e)=>{
+    
+    const removeItem = todo.filter((todo,id1) => {
+      return id1 !== e;
+    });
+  
+    setTodo(removeItem)
+  
+  }
+  const MakeEditable=()=>{
+    setEditable(!editable)
+  }
+  const updateTodo=(e)=>{
+    
+    const updateTodo=[...todo]
+    updateTodo[e]={
+      "Title":text,
+    "Content":content
+    }
+    
+    setTodo(updateTodo)
+    setEditable(false)
+  }
+
+
+  console.log(text,content);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={text}  onChange={e=>setText(e.target.value)} title="Titl!e" /> 
+      <input type="content"  value={content} onChange={e=>setContent(e.target.value)} title="Add Note" />
+      <button onClick={AddText}>Add Todo</button> 
+      {
+        todo.map((todo,id)=>{return (
+          <div style={{border:"10px solid black",margin:"20px",padding:"20px" ,width:"30%"}}>
+            <h1>{todo.Title}</h1>
+          <h2>{todo.Content}</h2>
+          <button onClick={MakeEditable}>{editable?"Cancel Edit":"Edit Todo"}</button> 
+          {editable?<>
+            <input type="text" value={text}  onChange={e=>setText(e.target.value)} title="Titl!e" /> 
+          <input type="content"  value={content} onChange={e=>setContent(e.target.value)} title="Add Note" />
+          <button onClick={()=>updateTodo(id)}>Edit Todo</button> 
+          </>:<></>}
+          <button onClick={()=>DeleteTodo(id)}>Delete Todo</button> 
+
+          </div>
+
+          
+        )
+          
+        })
+      }
     </div>
   );
 }
